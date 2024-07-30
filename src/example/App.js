fa3be50b10a9795736e59d5efd2bf2d985f68749
soyoung <soyoung.yang94@gmail.com>
2024-07-31 00:02:21 +0900

    🩹 fix: index 페이지 변경
    - GOOD BITE 로고로 변경
    - Sign up 링크 연결

--- src/example/App.js
+++ src/example/App.js
@@ -1,40 +1,45 @@
-// import logo from '../logo.svg';
-// import './App.css';
-//
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-//
-// export default App;
-
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation, Link } from 'react-router-dom';
 import Signup from '../components/Signup';
+import logo from '../good-bite-logo.png';
+import './App.css';
+
+const Header = () => (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Welcome to the GOOD BITE Project!
+      </p>
+      <Link
+          className="App-link"
+          to="/signup"
+      >
+      Sign up
+      </Link>
+    </header>
+);
+
+const Layout = ({ children }) => {
+  const location = useLocation();
+  const isSignupPage = location.pathname === '/signup';
+
+  return (
+      <div className="App">
+        {!isSignupPage && <Header />}
+        {children}
+      </div>
+  );
+};
 
 function App() {
   return (
       <Router>
-        <Routes>
-          <Route path="/signup" element={<Signup />} />
-          {/* 다른 경로를 추가할 수 있습니다. */}
-        </Routes>
+        <Layout>
+          <Routes>
+            <Route path="/signup" element={<Signup />} />
+            {/* 다른 경로를 추가할 수 있습니다. */}
+          </Routes>
+        </Layout>
       </Router>
   );
 }
