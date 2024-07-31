a4d39bf4bc20b56b642212c5a06ec962b4403e98
haeuni00 <leejw6311@naver.com>
2024-07-31 20:26:21 +0900

    ✨ feat: 고객 입장에서 가게 상세 페이지 구현
    - 가게 정보 포함
    - 영업시간 포함
    - 메뉴 목록 포함

--- src/example/App.js
+++ src/example/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation, Link } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+  Link
+} from 'react-router-dom';
 import Signup from '../components/Signup';
 import logo from '../good-bite-logo.png';
 import './App.css';
+import RegisterRestaurant from "../components/RegisterRestaurant";
+import RestaurantDetail from "../components/RestaurantDetail";
 
 const Header = () => (
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
+      <img src={logo} className="App-logo" alt="logo"/>
       <p>
         Welcome to the GOOD BITE Project!
       </p>
@@ -14,18 +22,20 @@ const Header = () => (
           className="App-link"
           to="/signup"
       >
-      Sign up
+        Sign up
       </Link>
     </header>
 );
 
-const Layout = ({ children }) => {
+const Layout = ({children}) => {
   const location = useLocation();
-  const isSignupPage = location.pathname === '/signup';
+  const isHeaderVisible = location.pathname !== '/signup'
+      && location.pathname !== '/register-restaurant'
+      && location.pathname !== '/restaurant-detail';
 
   return (
       <div className="App">
-        {!isSignupPage && <Header />}
+        {isHeaderVisible && <Header/>}
         {children}
       </div>
   );
@@ -36,7 +46,9 @@ function App() {
       <Router>
         <Layout>
           <Routes>
-            <Route path="/signup" element={<Signup />} />
+            <Route path="/signup" element={<Signup/>}/>
+            <Route path="/register-restaurant" element={<RegisterRestaurant/>}/>
+            <Route path="/restaurant-detail" element={<RestaurantDetail/>}/>
             {/* 다른 경로를 추가할 수 있습니다. */}
           </Routes>
         </Layout>
