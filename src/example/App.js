723d61124bc95b604e0d0199000ea966fc948f73
kim201621123 <66352581+kim201621123@users.noreply.github.com>
2024-07-31 18:00:13 +0900

    🩹 fix: 웨이팅 경로 추가

--- src/example/App.js
+++ src/example/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, useLocation, Link } from 'react-router-dom';
 import Signup from '../components/Signup';
+import Waiting from '../components/Waiting';
 import Login from '../components/Login';
 import logo from '../good-bite-logo.png';
 import './App.css';
@@ -17,16 +18,24 @@ const Header = () => (
       >
       Sign up
       </Link>
+
+      <Link
+          className="App-link"
+          to="/waiting"
+      >
+        make waiting
+      </Link>
     </header>
 );
 
 const Layout = ({ children }) => {
   const location = useLocation();
   const isSignupPage = location.pathname === '/signup';
+  const isWaiting = location.pathname === '/waiting';
 
   return (
       <div className="App">
-        {/*{!isSignupPage && <Header />}*/}
+        {!isSignupPage && !isWaiting && <Header />}
         {children}
       </div>
   );
@@ -38,6 +47,7 @@ function App() {
         <Layout>
           <Routes>
             <Route path="/signup" element={<Signup />} />
+            <Route path="/waiting" element={<Waiting />} />
             {/* 다른 경로를 추가할 수 있습니다. */}
             <Route path="/login" element={<Login />} />
           </Routes>
