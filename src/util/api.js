ad6912bfa52fe9aa553daaaea91c0ddfa0c74ba6
soyoung <soyoung.yang94@gmail.com>
2024-08-11 23:23:05 +0900

    🍻 Test: 배포 환경에 API_BASE_URL 이 적용되는지 확인

--- src/util/api.js
+++ src/util/api.js
@@ -1,6 +1,7 @@
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 export const fetchData = async (endpoint, options = {}) => {
+  console.log('API_BASE_URL:', API_BASE_URL);
   let accessToken = localStorage.getItem('accessToken');
   const refreshToken = localStorage.getItem('refreshToken');
 
