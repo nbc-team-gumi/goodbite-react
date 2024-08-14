28dcbea57ae0055b6707d338c54f205c72acda9b
soyoung <soyoung.yang94@gmail.com>
2024-08-14 14:40:11 +0900

    🛠️ build: DOMAIN_URL 환경변수 추가 - 웹 애플리케이션 사이트 도메인네임

--- src/components/KakaoLogin.js
+++ src/components/KakaoLogin.js
@@ -1,8 +1,9 @@
 const KAKAO_API_KEY = process.env.REACT_APP_API_KEY_KAKAO;
+const DOMAIN_URL = process.env.REACT_APP_DOMAIN_URL;
 
 export const kakaoLogin = async (isOwner) => {
   const kakaoClientId = KAKAO_API_KEY;
-  const redirectUri = `http://localhost:3000/kakao/callback`;
+  const redirectUri = `${DOMAIN_URL}/kakao/callback`;
   const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${kakaoClientId}&redirect_uri=${encodeURIComponent(redirectUri)}&state=${isOwner}`;
 
   window.location.href = kakaoAuthUrl;
