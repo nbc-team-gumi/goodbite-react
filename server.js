8a658cfff66becace0f93f616ed07ef7e23f97dd
soyoung <soyoung.yang94@gmail.com>
2024-08-20 22:38:13 +0900

    🛠️ build: http 버전 확인 로그 출력

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const port = 80;
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', function(req, res) {
+  const httpVersion = req.httpVersion; // 요청된 HTTP 버전 확인
+  console.log(`HTTP Version: ${httpVersion}`); // 콘솔에 HTTP 버전 출력
+
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
