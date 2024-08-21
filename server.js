43b2e918b321823a2dd72a65600d11f8e1b0f450
soyoung <soyoung.yang94@gmail.com>
2024-08-21 18:55:52 +0900

    ✨ feat: HTTP/2 설정

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const path = require('path');
+const http2 = require('http2');
+const fs = require('fs');
 const app = express();
-const port = 80;
+const port = 443;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', function(req, res) {
-  const httpVersion = req.httpVersion; // 요청된 HTTP 버전 확인
-  console.log(`HTTP Version: ${httpVersion}`); // 콘솔에 HTTP 버전 출력
-
+  const httpVersion = req.httpVersion;
+  console.log(`HTTP Version: ${httpVersion}`);
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+// HTTPS/HTTP2 서버를 생성
+const server = http2.createSecureServer({
+  key: fs.readFileSync(path.join(__dirname, 'goodbite.site-key.pem')),
+  cert: fs.readFileSync(path.join(__dirname, 'goodbite.site-crt.pem'))
+}, app);
+
+server.listen(port, () => {
+  console.log(`HTTP/2 Server running on port ${port}`);
+});
